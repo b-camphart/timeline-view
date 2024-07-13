5d1a04e45b66319c3b3351b9287b786d4e9ea973
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2024-07-13 13:40:46 -0700

    Remove dead code

--- src/timeline/layout/stage/CanvasStage.ts
+++ src/timeline/layout/stage/CanvasStage.ts
@@ -1,65 +1,6 @@
+import type { Scale } from "src/timeline/scale";
 import type { TimelineItem } from "../../Timeline";
 import { TimelineLayoutItem, type OffsetBox } from "./TimelineItemElement";
-
-// export function* renderStage(
-// 	this: CanvasRenderingContext2D,
-// 	viewport: {
-// 		width: number;
-// 		height: number;
-// 		centerValue: number;
-// 		scrollTop: number;
-// 	},
-// 	point: {
-// 		width: number;
-// 		marginX: number;
-// 		marginY: number;
-// 	},
-// 	scale: {
-// 		toPixels(value: number): number;
-// 		toValue(pixels: number): number;
-// 	},
-// 	sortedItems: TimelineItem[],
-// ) {
-// 	const pointRadius = point.width / 2;
-// 	const PI2 = 2 * Math.PI;
-
-// 	const defaultColor = this.fillStyle;
-
-// 	this.beginPath();
-// 	this.clearRect(0, 0, viewport.width, viewport.height);
-
-// 	const pointBounds = Array.from(layoutPoints(point, scale, sortedItems));
-// 	let maxY = 0;
-// 	for (const bounds of pointBounds) {
-// 		if (bounds.bottom > maxY) maxY = bounds.bottom;
-// 	}
-
-// 	const maxScroll = Math.max(0, maxY + point.marginY - viewport.height);
-// 	if (viewport.scrollTop > maxScroll) viewport.scrollTop = maxScroll;
-
-// 	let currentColor = this.fillStyle;
-
-// 	for (const bounds of pointBounds) {
-// 		const scrolledY = bounds.centerY - viewport.scrollTop;
-
-// 		const color = bounds.item.color() ?? defaultColor;
-
-// 		if (color !== currentColor) {
-// 			this.closePath();
-// 			this.fill();
-// 			this.beginPath();
-// 		}
-
-// 		this.fillStyle = color;
-
-// 		this.moveTo(bounds.right, scrolledY);
-// 		this.arc(bounds.centerX, scrolledY, pointRadius, 0, PI2);
-
-// 		yield new PointBounds(bounds.centerX, scrolledY, bounds.item, point);
-// 	}
-// 	this.closePath();
-// 	this.fill();
-// }
 
 export function renderLayout(
 	context: CanvasRenderingContext2D,
@@ -92,6 +33,7 @@ export function renderLayout(
 			context.closePath();
 			context.fill();
 			context.beginPath();
+			currentColor = color;
 		}
 
 		context.fillStyle = color;
@@ -122,10 +64,7 @@ export function layoutPoints(
 			vertical: number;
 		};
 	},
-	scale: {
-		toPixels(value: number): number;
-		toValue(pixels: number): number;
-	},
+	scale: Scale,
 	sortedItems: TimelineItem[],
 	previousLayout: TimelineLayoutItem[] = [],
 ) {
@@ -192,36 +131,4 @@ function findNextAvailableRow(
 	}
 
 	return lastXByRow.length;
-}
-
-export class PointBounds {
-	constructor(
-		public centerX: number,
-		public centerY: number,
-		public readonly item: TimelineItem,
-		private readonly point: { readonly width: number },
-	) {}
-
-	get x() {
-		return this.centerX - this.point.width / 2;
-	}
-	get y() {
-		return this.centerY - this.point.width / 2;
-	}
-	get left() {
-		return this.x;
-	}
-	get right() {
-		return this.centerX + this.point.width / 2;
-	}
-	get top() {
-		return this.y;
-	}
-	get bottom() {
-		return this.centerY + this.point.width / 2;
-	}
-
-	contains(x: number, y: number) {
-		return this.x <= x && x < this.right && this.y <= y && y < this.bottom;
-	}
 }
