dc0b0a856288cd9101a76ce992b0cb5c2a43b9d1
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2025-02-01 15:30:28 -0800

    bump to 1.4.0

--- src/obsidian/plugin/manifest.ts
+++ src/obsidian/plugin/manifest.ts
@@ -1,11 +1,11 @@
 export default {
-	"id": "timeline-view",
-	"name": "Timeline View",
-	"version": process.env.npm_package_version ?? "1.0.0",
-	"minAppVersion": "1.6.5",
-	"description": "Display your obsidian notes in a timeline, based on a given property.",
-	"author": "b-camphart",
-	"authorUrl": "https://github.com/b-camphart",
-	"fundingUrl": "https://buymeacoffee.com/b.camphart",
-	"isDesktopOnly": true
+	id: "timeline-view",
+	name: "Timeline View",
+	version: process.env.npm_package_version ?? "1.0.0",
+	minAppVersion: "1.8.3",
+	description: "Display your obsidian notes in a timeline, based on a given property.",
+	author: "b-camphart",
+	authorUrl: "https://github.com/b-camphart",
+	fundingUrl: "https://buymeacoffee.com/b.camphart",
+	isDesktopOnly: true,
 } as const;
