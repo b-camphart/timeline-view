4d65da89580c5af35c3e2b0ad2d4c0bde9836725
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2025-02-21 16:01:48 -0800

    dynamically pick minAppVersion from obsidian

--- src/obsidian/plugin/manifest.ts
+++ src/obsidian/plugin/manifest.ts
@@ -1,9 +1,22 @@
+import fs from "fs";
+import path from "path";
+
 export default {
 	id: "timeline-view",
 	name: "Timeline View",
 	version: process.env.npm_package_version ?? "1.0.0",
-	minAppVersion: "1.8.3",
-	description: "Display your obsidian notes in a timeline, based on a given property.",
+	minAppVersion: JSON.parse(
+		fs.readFileSync(
+			fs.realpathSync(
+				path.resolve("node_modules", "obsidian", "package.json")
+			),
+			{
+				encoding: "utf-8",
+			}
+		)
+	).version,
+	description:
+		"Display your obsidian notes in a timeline, based on a given property.",
 	author: "b-camphart",
 	authorUrl: "https://github.com/b-camphart",
 	fundingUrl: "https://buymeacoffee.com/b.camphart",
