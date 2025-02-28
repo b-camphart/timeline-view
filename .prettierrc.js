679ff55a59fab29f18b24f2eb9474c618b9c50f6
Brendan Campbell-Hartzell <b.camphart@gmail.com>
2025-02-28 14:47:26 -0800

    create prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+    "bracketSpacing": false,
+    "useTabs": true,
+    "printWidth": 120,
+    "useEditorConfig": true,
+    "semi": true
+}
